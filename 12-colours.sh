f78e0f243a4cc226f7a82e69ee7fd9d87b7b65f2
nagendra <rohittalisetty20@gmail.com>
2025-04-10 12:20:44 +0530

    shell script

--- 12-colours.sh
+++ 12-colours.sh
@@ -8,10 +8,10 @@ Y="\e[33m"
 VALIDATE(){ 
     if [ $1 -ne 0 ]
     then
-        echo "$2 ... $R FAILURE"
+        echo -e "$2 ... $R FAILURE"
         exit 1
     else
-        echo "$2 ... $G SUCCESS"
+        echo -e "$2 ... $G SUCCESS"
     fi 
 }
 
