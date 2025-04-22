8247227c7da57727389cc77e2c6f2d117d700fb8
nagendra <rohittalisetty20@gmail.com>
2025-04-22 12:26:11 +0530

    expense shell

--- 18-backup.sh
+++ 18-backup.sh
@@ -37,4 +37,16 @@ then
     USAGE
 fi
 
+if [ ! -d $SOURCE_DIR ]
+then
+    echo -e "$SOURCE_DIR Does not exist...Please check"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]
+then
+    echo -e "$DEST_DIR Does not exist...Please check"
+    exit 1
+fi
+
 echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE-name
